b9f98b969b78453ac528d274308438c70f94a0cf
red5191 <the_red51@outlook.com>
2024-11-15 03:31:57 +0300

    123

--- car.py
+++ car.py
@@ -1,29 +1,33 @@
 vin_dict = {
-    1234 : ('Skoda', 'Oktavia', 2013),
-    2345 : ('Lada', 'Kalina', 2005),
-    3456 : ('BMW', 'X5', 2019)
+    '1234' : ('Skoda', 'Oktavia', 2013),
+    '2345' : ('Lada', 'Kalina', 2005),
+    '3456' : ('BMW', 'X5', 2019)
 }
+
 
 
 class Car:
     def __init__(self, make, model, year):
-        make = vin_dict[vin[1]]
-        model = vin_dict[vin[2]]
-        year = vin_dict[vin[3]]
+        vin = None
+        self.make = make
+        self.model = model
+        self.year = year
 
     def get_info(self):
-        return print(f'Make: {make}, Model: {model}, Year: {year}')
+        return print(f'Make: {self.make}, Model: {self.model}, Year: {self.year}')
 
     @classmethod
     def from_vin(cls, vin):
-        return cls
-
+       return cls(vin_dict.get(vin)[0], vin_dict.get(vin)[1], vin_dict.get(vin)[2])
 
     @staticmethod
     def is_valid_year(year):
         if not 1886 < year < 2024:
             print(f'{year} неподходящий год выпуска')
+        else:
+            print(f'{year} подходящий год выпуска')
 
+car1 = Car.from_vin('1234')
 
-Car.from_vin(1234)
-Car
+car1.get_info()
+car1.is_valid_year(996)
